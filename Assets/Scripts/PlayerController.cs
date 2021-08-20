6cac03fe610b72f6466439597b0b8b28abf1e2a6
Berk Bekir Baran <berkbekirbaran@hotmail.com>
2021-08-20 14:25:47 +0300

    a
    a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,61 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Rigidbody2D playerRB;
+    public Animator playerAnimator;
+    public float moveSpeed = 1f;
+    
+
+    private bool facingRight = true;
+
+    Vector2 movement;
+
+
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
+        //playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Face Animation
+
+        //Left Right
+
+        if (playerRB.velocity.x < 0 && facingRight)
+        {
+            FlipFace();
+        }
+        else if (playerRB.velocity.x > 0 && !facingRight)
+        {
+            FlipFace();
+        }
+
+        //Top Down
+
+        if (playerRB.velocity.x < 0 && facingRight)
+        {
+            //TurnTop();
+        }
+        else if (playerRB.velocity.x > 0 && !facingRight)
+        {
+            //TurnDown();
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerRB.MovePosition(playerRB.position + movement  * moveSpeed);
+    }
+
+    private void FlipFace()
+    {
+
     }
 }
